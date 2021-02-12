e3782ae136d8d60f2b6334dbabdce396b6e472ee
Marcos Corsi <marcosvcorsi@gmail.com>
2021-02-11 21:28:57 -0300

    feat: add middlewares

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,16 +1,30 @@
 import { createAuctionService } from "../factories/auctions";
 import { created } from "../helpers/http";
 
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+
 const auctionsService = createAuctionService();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
 
-  const auction = await auctionsService.create({ title });
+  try {
+    const auction = await auctionsService.create({ title });
 
-  return created(auction);
+    return created(auction);
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
 
 
