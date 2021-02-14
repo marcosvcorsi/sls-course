a812f2b2e453d4f900d6c27f5e268300d9867846
Marcos Corsi <marcosvcorsi@gmail.com>
2021-02-13 23:10:39 -0300

    feat: add schema validation to get auctions

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -1,9 +1,29 @@
 import { createAuctionService } from "../factories/auctions";
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import { ok } from "../helpers/http";
 import { useMiddlewares } from "../helpers/middlewares";
+import { AUCTION_STATUS } from "../services/AuctionsService";
 
 const auctionsService = createAuctionService();
+
+const inputSchema = {
+  properties: {
+    queryStringParameters: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'string',
+          enum: Object.values(AUCTION_STATUS),
+          default: AUCTION_STATUS.OPEN
+        }
+      }
+    }
+  },
+  required: [
+    'queryStringParameters'
+  ]
+}
 
 async function getAuctions(event, context) {
   const { status } = event.queryStringParameters;
@@ -18,5 +38,8 @@ async function getAuctions(event, context) {
   }
 }
 
-export const handler = useMiddlewares(getAuctions)
+export const handler = useMiddlewares(getAuctions).use(validator({
+  inputSchema,
+  useDefaults: true
+}))
 
