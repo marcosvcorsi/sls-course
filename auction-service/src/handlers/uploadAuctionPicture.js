6547ba7e407808cfff7fe354bb9609f226efb94e
Marcos Corsi <marcosvcorsi@gmail.com>
2021-02-15 22:21:27 -0300

    fix: add seller validation do upload auction picture

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -5,12 +5,14 @@ import { createAuctionService } from '../factories/auctions';
 import NotFoundError from "../errors/NotFoundError";
 import { ok } from "../helpers/http";
 import UploadService from '../services/UploadService';
+import InvalidOperationError from '../errors/InvalidOperationError';
 
 const auctionsService = createAuctionService();
 const uploadService = new UploadService(process.env.AUCTIONS_BUCKET_NAME);
 
 async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   
   try {
 
@@ -20,6 +22,10 @@ async function uploadAuctionPicture(event, context) {
 
     const auction = await auctionsService.findById(id);
 
+    if(auction.seller !== email) {
+      throw new InvalidOperationError(`You can't update this auction picture`);
+    }
+
     const { Location } = await uploadService.upload(`${auction.id}.jpg`, buffer);
 
     const updatedAuction = await auctionsService.updateAuctionPicture(id, Location);
@@ -28,6 +34,8 @@ async function uploadAuctionPicture(event, context) {
   } catch(error) {
     if(error instanceof NotFoundError) {
       throw new createError.NotFound(error.message);
+    } else if(error instanceof InvalidOperationError) {
+      throw new createError.Forbidden(error.message) 
     } else {
       console.error(error);
       throw new createError.InternalServerError(error);
